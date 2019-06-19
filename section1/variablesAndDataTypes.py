d114f1b9b865a5b5b3ba4bd2f905471614a03fad
aakash <aakash.shinghal@gmail.com>
2019-06-19 23:05:12 +0530

    adding comments in file

--- section1/variablesAndDataTypes.py
+++ section1/variablesAndDataTypes.py
@@ -2,13 +2,18 @@ a = 10
 
 print(a)
 
+#naming a variable with name boolean and giving an integer value
 boolean = 2
 
+#assigning a positive integer to a variable
 posint = 10
 
+#nameing a variable zero and assigning zero to it
 zero = 0
 
+#assigning negative decimal value to the variable
 negFloat =  -10.0
 
+#reassignment
 initial = 10
 initial = 11
