e06aaa0a34288a11c72366773338698d30576f81
aakash <aakash.shinghal@gmail.com>
2019-06-20 01:07:11 +0530

    adding string methods

---
+++ section1/String Methods.py
@@ -0,0 +1,39 @@
+"""
+len() and str() practice:
+1.create a variable and assign it the string "Python"
+2.create another variable and assign it the length of the string assigned to the variable in step 1
+3.create a variable and use string slicing and len() to assign it the length of the slice "yth" from the string assigned
+ to the variable from step 1
+4.create a variable and assign it the float 1.32
+5.create a variable and assign it the string "2" from the float assigned to the variable from the last problem (use the
+ str() string method for this)
+"""
+# type your code for "len() and str() practice" below this single line comment and above the one below it --------------
+# ----------------------------------------------------------------------------------------------------------------------
+
+var1 = "Python"
+var2 = len(var1)
+var3 = len(var1[1:4])
+var4 = 1.32
+#var5 = 1.3(str(2))
+
+print(var1)
+print(var2)
+print(var3)
+print(var4)
+#print(var5)
+
+"""
+.upper() and .lower() practice:
+1.create a variable and assign it the string "upper" changed to "UPPER" using .upper()
+2.create a variable and assign it the string "owe" from "LOWER" using string slicing and .lower()
+"""
+# type your code for ".upper() and .lower() practice" below this single line comment and above the one below it --------
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+var6= "upper".upper()
+var7 = "LOWER".lower()
+
+print(var6)
+print(var7)
