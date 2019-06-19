26cbdc88b51f333990c1f2424e184a7f2f18379e
aakash <aakash.shinghal@gmail.com>
2019-06-19 23:12:27 +0530

    adding some lines in datatype file

--- section1/variablesAndDataTypes.py
+++ section1/variablesAndDataTypes.py
@@ -2,6 +2,30 @@ a = 10
 
 print(a)
 
+#create  a variable called intVal and asign it an integer value
+intVal = 3
+
+#create  a variable called floatVal and asign it a float value
+floatVal = 3.0
+
+#create  a variable called boolVal and asign it a boolean value
+boolVal = True
+
+#reassign different boolean value
+boolVal = False
+
+#display integer value
+print(intVal)
+
+#display float value
+print(floatVal)
+
+#display boolean value
+print(boolVal)
+
+#use print to output boolean value
+print(boolVal)
+
 #naming a variable with name boolean and giving an integer value
 boolean = 2
 
