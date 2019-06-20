fbb83beb01086715e143db83662577478c51834b
aakash <aakash.shinghal@gmail.com>
2019-06-20 10:04:27 +0530

    added flow control and comparator file

---
+++ section1/FlowControlAndComparator.py
@@ -0,0 +1,41 @@
+"""
+Flow Control and Comparators:
+1.create a variable and assign to it an expression that is true using the greater than comparator
+2.create a variable and assign to it an expression that is false using the greater than comparator
+3.create a variable and assign to it an expression that is true using the greater than or equal to comparator
+4.create a variable and assign to it an expression that is false using the greater than or equal to comparator
+5.create a variable and assign to it an expression that is true using the less than comparator
+6.create a variable and assign to it an expression that is false using the less than comparator
+7.create a variable and assign to it an expression that is true using the less than or equal to comparator
+8.create a variable and assign to it an expression that is false using the less than or equal to comparator
+9.create a variable and assign to it an expression that is true using the equal to comparator
+10.create a variable and assign to it an expression that is false using the equal to comparator
+11.create a variable and assign to it an expression that is true using the not equal to comparator
+12.create a variable and assign to it an expression that is false using the not equal to comparator
+"""
+
+var1 = 5>3
+var2 = 3>5
+var3 = 3>=3
+var4 = 2>=3
+var5 = 3<5
+var6 = 5<3
+var7 = 3<=5
+var8 = 5<=2
+var9 = 5==5
+var10 = 5==4
+var11 = 5!=6
+var12 = 6!=6
+
+print(var1)
+print(var2)
+print(var3)
+print(var4)
+print(var5)
+print(var6)
+print(var7)
+print(var8)
+print(var9)
+print(var10)
+print(var11)
+print(var12)
