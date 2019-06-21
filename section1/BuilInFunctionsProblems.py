62b212ee4e50acdb35fa7246e371e0b14aafb589
aakash <aakash.shinghal@gmail.com>
2019-06-21 16:24:12 +0530

    added built in functions

---
+++ section1/BuilInFunctionsProblems.py
@@ -0,0 +1,32 @@
+"""
+abs() and type():
+1.use abs() to get and print the absolute value of a positive number
+2.use abs() to get and print the absolute value of a negative number
+3.use type() to get and print the type of a float
+4.use type() to get and print the type of an integer
+5.use type() to get and print the type of a boolean value
+6.use type() to get and print the type of a string
+"""
+# ----------------------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
+
+print(abs(7))
+print(abs(-7))
+print(type(6.5))
+print(type(6))
+print(type(True))
+print(type("Hello"))
+"""
+max() and min():
+1.use max() to get and print the largest of inputs of your choosing that are all integers
+2.use max() to get and print the alphabetical maximum of inputs of your choosing that are all strings
+3.use min() to get and print the smallest of inputs of your choosing that are all floats
+4.use min() to get and print the alphabetical minimum of inputs of your choosing that are all strings
+"""
+# ----------------------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
+
+print(max(1,2,3,4,5,6))
+print(max('a','b','c','d','e','f'))
+print(min(1,2,3,4,5,6))
+print(min('a','b','c','d','e','f'))
