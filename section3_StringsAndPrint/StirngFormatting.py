bee260c8221f389816fae4d0bcd7eb1028f6c87e
aakash <aakash.shinghal@gmail.com>
2019-06-25 09:22:41 +0530

    aadding string formating methods

---
+++ section3_StringsAndPrint/StirngFormatting.py
@@ -0,0 +1,8 @@
+city = "Seatle"
+state = "Washington"
+
+print("The seahwaks are from %s, %s"%(city,state))
+
+numbers = [4,5,6]
+print("Numbers are written in string format %s,%s,%s"%(numbers[0],numbers[1],numbers[2]))
+print("Numbers are written in string format {0},{1},{2}".format(numbers[0],numbers[1],numbers[2]))
