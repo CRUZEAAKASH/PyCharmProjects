b195df00577bc09664e1206dea919e7d54d74fc6
aakash <aakash.shinghal@gmail.com>
2019-06-30 15:36:57 +0530

    added set operations

---
+++ ObjectOrientedProgramming/setOperation.py
@@ -0,0 +1,23 @@
+numbers = {1, 2, 3, 4, 5, 6}
+numbers2 = {4, 5, 6, 7, 8, 9}
+print(numbers)
+print(numbers2)
+
+#addition operation
+numbers.add(9)
+print(numbers)
+numbers.add(5)
+print(numbers)
+
+#removal operation
+numbers.remove(2)
+print(numbers)
+
+#union operation
+print(numbers | numbers2)
+
+#intersection operator
+print(numbers & numbers2)
+
+#Minus Operator
+print(numbers - numbers2)
