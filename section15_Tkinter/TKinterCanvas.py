c71b451ec1b1dbf14702058e61c2848208a6412f
aakash <aakash.shinghal@gmail.com>
2019-07-15 12:39:23 +0530

    modified git canvas

--- section15_Tkinter/TKinterCanvas.py
+++ section15_Tkinter/TKinterCanvas.py
@@ -5,6 +5,6 @@ window = Tk()
 canvas = Canvas(window, width=200, height=100)
 canvas.pack()
 lin1 = canvas.create_line(0, 0, 50, 100)
-line2 = canvas.create_line(10, 90, 20, 0)
+line2 = canvas.create_line(10, 90, 20, 0, fill="red")
 
 window.mainloop()
