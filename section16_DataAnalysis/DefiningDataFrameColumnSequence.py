8e2cdea6074d815f7cc067d009464ee2e88589a0
aakash <aakash.shinghal@gmail.com>
2019-07-17 08:03:23 +0530

    added dataframe sequencing

---
+++ section16_DataAnalysis/DefiningDataFrameColumnSequence.py
@@ -0,0 +1,9 @@
+from pandas import DataFrame
+
+data = {'Name': ['John', 'Tom', 'Rob'],
+        'Age': [10, 20, 30],
+        'Salary': [2500, 3500, 4500]}
+
+frame = DataFrame(data, columns=['Age', 'Salary', 'Name'])
+
+print(frame)
